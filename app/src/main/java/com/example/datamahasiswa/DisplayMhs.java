02fc5d6a4549f5eda0d54972413310f1d6d66b46
Mochammad Ifan Rizky Kurniawan <rizkykurniawan.ifan463@gmail.com>
2020-04-16 20:08:14 +0700

    add some code in class DisplayMhs

--- app/src/main/java/com/example/datamahasiswa/DisplayMhs.java
+++ app/src/main/java/com/example/datamahasiswa/DisplayMhs.java
@@ -2,20 +2,24 @@ package com.example.datamahasiswa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class DisplayMhs extends AppCompatActivity {
     int from_Where_I_Am_Coming = 0;
     private DBHelper mydb;
 
-    TextView nomhs;
-    TextView phone;
-    TextView nama;
+    EditText nomhs;
+    EditText phone;
+    EditText nama;
 
     int id_To_Update = 0;
 
@@ -23,17 +27,17 @@ public class DisplayMhs extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_mhs);
-        nomhs = (TextView) findViewById(R.id.editTextNim);
-        nama = (TextView) findViewById(R.id.editTextName);
-        phone = (TextView) findViewById(R.id.editTextPhone);
+        nomhs = (EditText) findViewById(R.id.editTextNim);
+        nama = (EditText) findViewById(R.id.editTextName);
+        phone = (EditText) findViewById(R.id.editTextPhone);
 
         mydb = new DBHelper(this);
 
         Bundle extras = getIntent().getExtras();
-        if(extras !=null) {
+        if (extras != null) {
             int Value = extras.getInt("id");
 
-            if(Value>0) {
+            if (Value > 0) {
                 //means this is the view part not the add contact part.
                 Cursor rs = mydb.getData(Value);
                 id_To_Update = Value;
@@ -49,20 +53,40 @@ public class DisplayMhs extends AppCompatActivity {
                 Button b = (Button) findViewById(R.id.button1);
                 b.setVisibility(View.INVISIBLE);
 
-                nomhs.setText((CharSequence)no);
+                nomhs.setText((CharSequence) no);
                 nomhs.setFocusable(false);
                 nomhs.setClickable(false);
 
-                nama.setText((CharSequence)nam);
+                nama.setText((CharSequence) nam);
                 nama.setFocusable(false);
                 nama.setClickable(false);
 
-                phone.setText((CharSequence)phon);
+                phone.setText((CharSequence) phon);
                 phone.setFocusable(false);
                 phone.setClickable(false);
             }
         }
     }
+
+    public void run(View view) {
+        if (nomhs.getText().toString().equals("") ||
+                nama.getText().toString().equals("") ||
+                phone.getText().toString().equals("")) {
+            Toast.makeText(getApplicationContext(),
+                    "Data Harus Diisi Semua !", Toast.LENGTH_LONG).show();
+        } else {
+            mydb.insertContact(
+                    nomhs.getText().toString(),
+                    nama.getText().toString(),
+                    phone.getText().toString());
+            Toast.makeText(getApplicationContext(),
+                    "Insert data berhasil", Toast.LENGTH_LONG).show();
+
+            Intent i = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(i);
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu (Menu menu) {
